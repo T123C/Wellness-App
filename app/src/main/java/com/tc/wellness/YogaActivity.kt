d1dacdb66a344ba80f691a03d3b21e37d41f7b6d
T123C <tc2066@bath.ac.uk>
2022-04-06 14:14:00 +0100

    New button for Yoga Activity

--- app/src/main/java/com/tc/wellness/YogaActivity.kt
+++ app/src/main/java/com/tc/wellness/YogaActivity.kt
@@ -2,6 +2,8 @@ package com.tc.wellness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.tc.wellness.databinding.ActivityYogaBinding
 
 class YogaActivity : AppCompatActivity() {
@@ -15,5 +17,26 @@ class YogaActivity : AppCompatActivity() {
         setContentView(binding.root)
         //Setting the custom title for action bar
         title = "Time For Yoga"
+        button()
+    }
+
+    private fun button() {
+
+        val dialogPopUp = AlertDialog.Builder(this)
+            .setTitle("Break Time!")
+            .setMessage("Would you like some inspiration?")
+            .setIcon(R.drawable.ic_smiley_24)
+            .setPositiveButton("Oh Yes") { dialogInterface, i ->
+                Toast.makeText(
+                    this,
+                    "Peace comes from within, do some yoga to achieve it",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setNegativeButton("Nah") { dialogInterface, i ->
+                Toast.makeText(this, "Have some coffee then!", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        binding.inspiration.setOnClickListener { dialogPopUp.show() }
     }
 }
